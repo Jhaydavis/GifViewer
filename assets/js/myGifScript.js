2fb5f911df89ccdabc0096fafeb83c396636405e
Justlin Davis <jhaydavis@Justlins-MacBook-Pro.local>
2017-09-01 21:47:54 -0500

    Triva Game ver2

--- assets/js/myGifScript.js
+++ assets/js/myGifScript.js
@@ -7,7 +7,7 @@ function displayGifInfo() {
     var gif = $(this).attr("data-gif");
     var gifsToLoad = 10;
     var state = $(this).attr("data-state");
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
         gif + "&api_key=dc6zaTOxFJmzC&limit=10";
 
     $.ajax({
