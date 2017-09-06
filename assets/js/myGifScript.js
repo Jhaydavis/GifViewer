5b6f6eb342d6a5f75d0d65102053006d71600d87
Justlin Davis <jhaydavis@Justlins-MacBook-Pro.local>
2017-09-06 13:00:54 -0500

    GifViewer V3

--- assets/js/myGifScript.js
+++ assets/js/myGifScript.js
@@ -3,10 +3,11 @@ var gifs = ["Game of Thrones", "Epic Fail", "Slippery", "Punched", "Drunk"];
 // set up listener for clicking a button
 function displayGifInfo() {
     $(".item").empty();
+   
 
     var gif = $(this).attr("data-gif");
-    var gifsToLoad = 10;
-    var state = $(this).attr("data-state");
+    var gifsToLoad = 2;
+    //var state = $(this).data("data-state", "playing");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
         gif + "&api_key=dc6zaTOxFJmzC&limit=10";
 
@@ -16,30 +17,34 @@ function displayGifInfo() {
         })
         .done(function (response) {
 
-            console.log(response);
+
             var results = response.data;
 
-            console.log(state);
 
             for (var i = 0; i <= gifsToLoad; i++) {
                 var gifDiv = $("<div class='item'>");
-                
+
 
                 var rating = results[i].rating;
                 var p = $("<h4>").html("Rating: " + rating);
                 var h = $("<h3>").html("Category: " + gif);
 
                 var gifImage = $("<img>");
+
                 gifImage.attr("src", results[i].images.fixed_height.url);
                 gifImage.addClass(gifImage);
+                var state = gifImage.data("data-state", "isPlaying")
+                console.log(state);
 
-                
+
+
+
                 gifDiv.append(gifImage);
                 gifDiv.append(h);
                 gifDiv.append(p);
-                
-                
-                
+                // if click of .this class of item -> show still imgage
+
+
                 $("#gifs-appear-here").prepend(gifDiv);
 
             };
@@ -49,10 +54,14 @@ function displayGifInfo() {
 
 // I'm trying to detect when someone clicks on the gif image not working
 function toggleGif() {
-    $(this).show(gifImage.attr("src", results[i].images.fixed_height_still.url));
+
+    alert(state);
 };
 
+
 function renderButtons() {
+
+
 
     $("#buttons-view").empty();
     // Looping through the array of gifs
@@ -63,6 +72,7 @@ function renderButtons() {
         addedGif.attr("data-gif", gifs[i]);
         addedGif.text(gifs[i]);
         $("#buttons-view").append(addedGif);
+        $("#gif-input").val(" ");
     };
 };
 
@@ -70,11 +80,13 @@ function renderButtons() {
 
 $("#add-gif").on("click", function (event) {
     event.preventDefault();
+
     // This line grabs the input from the textbox
     var gif = $("#gif-input").val().trim();
 
     // Adding movie from the textbox to our array
     gifs.push(gif);
+    $("#gif-input").val(" ");
 
     // Calling renderButtons which handles the processing of our movie array
     renderButtons();
@@ -82,3 +94,4 @@ $("#add-gif").on("click", function (event) {
 renderButtons();
 $(document).on("click", ".gif", displayGifInfo);
 $(document).on("click", ".item", toggleGif);
+
