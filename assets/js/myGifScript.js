1112ba99125ded944aee893446440fe79486dcf1
Justlin Davis <jhaydavis@Justlins-MacBook-Pro.local>
2017-09-06 13:18:39 -0500

    GifViewer V4

--- assets/js/myGifScript.js
+++ assets/js/myGifScript.js
@@ -6,7 +6,7 @@ function displayGifInfo() {
    
 
     var gif = $(this).attr("data-gif");
-    var gifsToLoad = 2;
+    var gifsToLoad = 10;
     //var state = $(this).data("data-state", "playing");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
         gif + "&api_key=dc6zaTOxFJmzC&limit=10";
